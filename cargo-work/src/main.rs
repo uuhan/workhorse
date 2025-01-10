2dad25f83623068cf809e731f84c8abe9bdf7c9e
uuhan <xuminhui189@gmail.com>
2025-01-10 14:21:41 +0800

    cargo-work: indicatif progress bar

--- cargo-work/src/main.rs
+++ cargo-work/src/main.rs
@@ -1,5 +1,9 @@
 use cargo_work::Build;
 use clap::{Parser, Subcommand};
+use indicatif::{ProgressBar, ProgressState, ProgressStyle};
+use std::thread;
+use std::time::Duration;
+use std::{cmp::min, fmt::Write};
 
 #[allow(clippy::large_enum_variant)]
 #[derive(Debug, Subcommand)]
@@ -7,8 +11,10 @@ use clap::{Parser, Subcommand};
 pub enum Opt {
     #[command(name = "build", alias = "b")]
     Build(Build),
-    #[command(name = "push", alias = "p")]
+    #[command(name = "push", alias = "p", about = "Push to remote repository")]
     Push,
+    #[command(name = "pull", alias = "l", about = "Fetch artifacts from horsed")]
+    Pull,
 }
 
 #[derive(Debug, Parser)]
@@ -31,6 +37,25 @@ fn main() -> anyhow::Result<()> {
         Cli::Opt(opt) | Cli::Cargo(opt) => match opt {
             Opt::Build(build) => println!("{:?}", build),
             Opt::Push => println!("git push"),
+            Opt::Pull => {
+                let mut downloaded = 0;
+                let total_size = 23123123;
+
+                let pb = ProgressBar::new(total_size);
+                pb.set_style(ProgressStyle::with_template("{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({eta})")
+                    .unwrap()
+                    .with_key("eta", |state: &ProgressState, w: &mut dyn Write| write!(w, "{:.1}s", state.eta().as_secs_f64()).unwrap())
+                    .progress_chars("#>-"));
+
+                while downloaded < total_size {
+                    let new = min(downloaded + 223211, total_size);
+                    downloaded = new;
+                    pb.set_position(new);
+                    thread::sleep(Duration::from_millis(8));
+                }
+
+                pb.finish_with_message("downloaded");
+            }
         },
     }
 
