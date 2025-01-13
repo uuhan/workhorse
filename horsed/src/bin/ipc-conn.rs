821f871873ead7a9b04fdb1d67e74ed4840deea6
uuhan <xuminhui189@gmail.com>
2025-01-13 23:13:06 +0800

    horsed: 使用 snake_case 格式

--- horsed/src/bin/ipc-conn.rs
+++ horsed/src/bin/ipc-conn.rs
@@ -12,7 +12,20 @@ fn main() {
 
         let (_, mut sender) = conn.split();
 
-        sender.write_all(b"Hello from client!\n").await;
+        sender
+            .write_all(
+                serde_json::to_string(&data::Data::GitHook {
+                    kind: "pre-commit".to_string(),
+                    args: vec!["file1.txt".to_string(), "file2.txt".to_string()],
+                })
+                .unwrap()
+                .as_bytes(),
+            )
+            .await;
+
+        sender
+            .write_all(serde_json::to_string(&data::Data::Exit).unwrap().as_bytes())
+            .await;
 
         Ok(())
     });
--- horsed/src/ipc/data.rs
+++ horsed/src/ipc/data.rs
@@ -2,6 +2,7 @@ use serde::{Deserialize, Serialize};
 
 /// Horsed Ipc 数据
 #[derive(Serialize, Deserialize, Clone)]
+#[serde(rename_all = "snake_case")]
 pub enum Data {
     /// Git Hooks 事件
     GitHook { kind: String, args: Vec<String> },
