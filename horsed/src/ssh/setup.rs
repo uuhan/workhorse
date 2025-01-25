72c450405c9a04a1eefebb746c17067e019faf21
uuhan <xuminhui189@gmail.com>
2025-01-25 17:30:22 +0800

    horsed: a setup server for first setup

--- horsed/src/ssh/setup.rs
+++ horsed/src/ssh/setup.rs
@@ -1,12 +1,22 @@
+/// SETUP MODE:
+///
+/// THIS CODE IS NOT USED IN THE PRODUCTION ENVIRONMENT. IT IS ONLY USED TO SET UP THE SERVER.
+///
 use crate::db::entity::{
     prelude::{SshPk, User},
     ssh_pk, user,
 };
-use crate::prelude::DB;
+use crate::prelude::*;
+use anyhow::Context;
+use futures::future::FutureExt;
 use russh::keys::ssh_key::PublicKey;
+use russh::ChannelId;
 use russh::{server::*, MethodSet};
-use sea_orm::ActiveValue::{self, NotSet, Set, Unchanged};
-use sea_orm::{EntityTrait, ModelTrait};
+use sea_orm::{
+    ActiveModelTrait,
+    ActiveValue::Set,
+    ColumnTrait, QueryFilter, TransactionTrait, {EntityTrait, ModelTrait},
+};
 
 struct SetupServer {}
 
@@ -22,42 +32,79 @@ impl Server for SetupServer {
 
 #[async_trait::async_trait]
 impl Handler for SetupServer {
-    type Error = anyhow::Error;
+    type Error = HorseError;
 
     async fn auth_publickey_offered(
         &mut self,
         action: &str,
         pk: &PublicKey,
     ) -> Result<Auth, Self::Error> {
-        tracing::info!("Auth Publickey Offered: {}, {:?}", action, pk.to_openssh());
+        tracing::info!("PK Offered: [{}] {:?}", action, pk.to_openssh());
         Ok(Auth::Accept)
     }
 
-    async fn auth_publickey(&mut self, user: &str, pk: &PublicKey) -> anyhow::Result<Auth> {
+    async fn auth_publickey(&mut self, user: &str, pk: &PublicKey) -> HorseResult<Auth> {
         let alg = pk.algorithm();
         #[allow(deprecated)]
         let key = base64::encode(&pk.to_bytes()?);
-        let user_name = user;
 
         let conn = DB.clone();
 
-        let user = user::ActiveModel {
-            name: Set(user.to_string()),
-            ..Default::default()
-        };
+        let user_name = user.to_string();
+        if let Err(err) = conn
+            .transaction::<_, (), HorseError>(move |txn| {
+                async move {
+                    let name = user_name.clone();
+                    let user = user::ActiveModel {
+                        name: Set(name),
+                        ..Default::default()
+                    };
 
-        let ir = User::insert(user).exec(&conn).await?;
-        let id = ir.last_insert_id;
+                    user.save(txn).await?;
 
-        let auth = ssh_pk::ActiveModel {
-            alg: Set(alg.to_string()),
-            key: Set(key),
-            user_id: Set(id),
-            ..Default::default()
-        };
+                    let res = User::find()
+                        .filter(user::Column::Name.eq(user_name))
+                        .one(txn)
+                        .await?
+                        .context("bad txn")?;
 
-        tracing::info!("Setup As: {}", user_name);
+                    let auth = ssh_pk::ActiveModel {
+                        alg: Set(alg.to_string()),
+                        key: Set(key),
+                        user_id: Set(res.id),
+                    };
+
+                    auth.save(txn).await?;
+                    Ok(())
+                }
+                .boxed()
+            })
+            .await
+        {
+            tracing::error!("DB ERROR: {err:?}");
+        }
+
+        tracing::info!("Setup As: {}", user);
 
         Ok(Auth::Accept)
+    }
+
+    async fn shell_request(
+        &mut self,
+        channel: ChannelId,
+        session: &mut Session,
+    ) -> Result<(), Self::Error> {
+        session.channel_success(channel)?;
+        Ok(())
+    }
+
+    async fn exec_request(
+        &mut self,
+        channel: ChannelId,
+        command: &[u8],
+        session: &mut Session,
+    ) -> Result<(), Self::Error> {
+        session.channel_success(channel)?;
+        Ok(())
     }
 }
