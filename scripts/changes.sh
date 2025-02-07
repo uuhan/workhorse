c77414ff4a73f7b91a3e96b157f73e1768721065
uuhan <xuminhui189@gmail.com>
2025-02-07 15:41:50 +0800

    scripts/change.sh

--- scripts/changes.sh
+++ scripts/changes.sh
@@ -3,4 +3,16 @@
 LatestTag=$(git tag | tail -n 1)
 Tag="${1:-$LatestTag}"
 
-git log --pretty='- %B' "${Tag}"...HEAD | sed -e /^$/d | uniq
+if [[ -n "$1" ]]; then
+  git log --pretty='- %B' "${Tag}"...HEAD | sed -e /^$/d | uniq
+else
+  V1=$(echo "$Tag" | cut -d'.' -f 1)
+  V2=$(echo "$Tag" | cut -d'.' -f 2)
+  V3=$(echo "$Tag" | cut -d'.' -f 3)
+
+  V3N=$((V3 + 1))
+
+  echo '###' "$V1.$V2.$V3N"
+  echo
+  git log --pretty='- %B' "${Tag}"...HEAD | sed -e /^$/d | uniq
+fi
