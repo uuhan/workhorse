608e8ec5048ced3ab20708110f6e8819b303a41f
uuhan <xuminhui189@gmail.com>
2025-06-24 11:29:35 +0800

    zebra: init

---
+++ zebra/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { invoke } from "@tauri-apps/api/core";
+import "./App.css";
+
+function App() {
+  return (
+    <main className="container">
+      <h1>Just a Zebra</h1>
+    </main>
+  );
+}
+
+export default App;
